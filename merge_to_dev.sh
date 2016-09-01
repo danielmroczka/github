b7dd5e5bef82cfb6a283cdf3ca60af64bbd6ce67
yale <danielmroczka@users.noreply.github.com>
2016-09-01 16:31:19 +0200

    Update merge_to_dev.sh

--- merge_to_dev.sh
+++ merge_to_dev.sh
@@ -4,8 +4,10 @@ fi
 
 export GIT_COMMITTER_EMAIL=daniel.mroczka@gmail.com
 export GIT_COMMITTER_NAME=danielmroczka
+export GH_TOKEN=ebb70d5af5c8e8f2c6d2e61c92d6731acc70acb5
 
 git fetch 
+git config --global push.default simple
 git checkout -b origin/dev || exit
 git merge "$TRAVIS_COMMIT" || exit
 git push
