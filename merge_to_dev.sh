0f05db9bc0423b7b213a9c99e70233451e8de3a8
yale <danielmroczka@users.noreply.github.com>
2016-09-01 16:12:30 +0200

    Update merge_to_dev.sh

--- merge_to_dev.sh
+++ merge_to_dev.sh
@@ -5,6 +5,8 @@ fi
 export GIT_COMMITTER_EMAIL=daniel.mroczka@gmail.com
 export GIT_COMMITTER_NAME=danielmroczka
 
+git remote add origin https://github.com/danielmroczka/github.git
+git fetch 
 git checkout dev || exit
 git merge "$TRAVIS_COMMIT" || exit
 git push
